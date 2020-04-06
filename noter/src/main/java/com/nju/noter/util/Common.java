9d95a1ced895ac4dfaee6c91f2bde3c4a1bb39b8
chinalienming <728385437@qq.com>
2020-04-07 00:48:28 +0800

    Common.java

---
+++ noter/src/main/java/com/nju/noter/util/Common.java
@@ -0,0 +1,8 @@
+package com.nju.noter.util;
+
+public class Common {
+
+    public final static String NOTEBOOK_DEFAULT_NAME = "default";
+
+    public final static String NOTEBOOK_DEFAULT_DESCRIPTION = "";
+}
